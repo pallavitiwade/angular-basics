e814cefe5a6b999eec2cbd01dfa33f63958b6f84
pallavitiwade <tiwdepallavi97@gmail.com>
2026-04-13 17:41:16 +0530

    initial commit

---
+++ src/app/shared/components/product/product.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+selector:'app-product',
+templateUrl: './product.component.html',
+styleUrls:['./product.component.scss']
+
+
+})
+export class ProductComponent{
+    PRODUCT_NAME:string="samsung M32";
+    PRODUCT_ID:number=123;
+    STUDENT_NAME:string="rakesh sharma";
+    ROLL_NO:number=25;
+    TEACHER_NAME:string="ashish sharma";
+    SUBJECT_NAME:string="Math";
+    DISH_NAME:string="Gajar halwa";
+   STUDENT:string="rudra";
+   RANK:string="topper"; 
+
+
+
+
+
+
+}
---
+++ src/app/shared/components/products/products.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'app-products',
+    templateUrl:'products.component.html',
+    styleUrls:['./products.component.scss']
+
+})
+
+export class ProductsComponent{
+testHtml:string='<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>'
+test:string='<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>'
+html:string='<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>'
+
+userName='john Doe'
+userEmail="johndoe@gmail.com"
+userContact=9874563478
+
+imgSrc='https://tse1.mm.bing.net/th/id/OIP.OEsew1xTAbTvqJ8ln2aJrgHaH3?pid=Api'
+imgSrc1='https://w7.pngwing.com/pngs/270/103/png-transparent-typescript-language-javascript-static-type-typescript-logo-frontend-3d-icon.png'
+imgSrc2='https://1000logos.net/wp-content/uploads/2020/09/JavaScript-Logo.png'
+imgSrc3='https://tse1.mm.bing.net/th/id/OIP.D1gvhsEUvjffVQ6sh793PQHaF5?pid=Api'
+imgSrc4='https://w7.pngwing.com/pngs/201/90/png-transparent-logo-html-html5.png'
+
+
+}
