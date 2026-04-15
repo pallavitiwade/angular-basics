599da28383421012485b0fb334b7da8299a7955c
pallavitiwade <tiwdepallavi97@gmail.com>
2026-04-15 23:40:25 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './shared/components/product/product.component';
 import { ProductsComponent } from './shared/components/products/products.component';
+import { InputComponent } from './shared/components/input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
-    ProductsComponent
+    InputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,100 @@
+import { Component, ElementRef, ViewChild } from "@angular/core";
+
+
+@Component({
+    selector:'app-Input',
+    templateUrl:'./input.component.html',
+    styleUrls:['./input.component.scss']
+
+})
+
+export class InputComponent{
+    onSearch(eve:Event){
+        let val=(eve.target as HTMLInputElement).value
+        console.log(val)
+    }
+
+
+    
+    onSearch1(ele:HTMLInputElement){
+        let val=ele.value
+        console.log(`searched ${val}`);
+        ele.value=''
+
+    }
+
+
+    @ViewChild('city')
+    pn!:ElementRef
+    onSearch2(){
+        console.log(this.pn.nativeElement.value);
+        this.pn.nativeElement.value=''
+     
+    }
+    @ViewChild('actor')
+    pn1!:ElementRef
+    onSearch3(){
+        console.log(this.pn1.nativeElement.value);
+        this.pn1.nativeElement.value=''
+     
+    }
+    @ViewChild('singer')
+    pns!:ElementRef
+    onSearch4(){
+        console.log(this.pns.nativeElement.value);
+        this.pns.nativeElement.value=''
+     
+    }
+        @ViewChild('politician')
+    pn4!:ElementRef
+    onSearch5(){
+        console.log(this.pn4.nativeElement.value);
+        this.pns.nativeElement.value=''
+     
+    }
+    @ViewChild('rich')
+    pn6!:ElementRef
+    onSearch6(){
+        console.log(this.pn6.nativeElement.value);
+        this.pn6.nativeElement.value=''
+     
+    }
+    @ViewChild('artist')
+    pn7!:ElementRef
+    onSearch7(){
+        console.log(this.pn7.nativeElement.value);
+        this.pn7.nativeElement.value=''
+     
+    }
+    @ViewChild('cartoon')
+    pn8!:ElementRef
+    onSearch8(){
+        console.log(this.pn8.nativeElement.value);
+        this.pn8.nativeElement.value=''
+     
+    }
+    @ViewChild('film')
+    pn9!:ElementRef
+    onSearch9(){
+        console.log(this.pn9.nativeElement.value);
+        this.pn9.nativeElement.value=''
+     
+    }
+    @ViewChild('web')
+    pn10!:ElementRef
+    onSearch10(){
+        console.log(this.pn10.nativeElement.value);
+        this.pn10.nativeElement.value=''
+     
+    }
+    @ViewChild('lawyer')
+    pn11!:ElementRef
+    onSearch11(){
+        console.log(this.pn11.nativeElement.value);
+        this.pn11.nativeElement.value=''
+     
+    }
+
+    
+}
+
